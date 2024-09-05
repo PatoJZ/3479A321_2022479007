0993a0ff521f16e4d6706af2271c7c46f3dba2b2
PatoJZ <patojimenez08@gmail.com>
2024-09-05 10:41:57 -0400

    clases

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:logger/logger.dart';
 
+import 'pages/my_home_page.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -19,74 +21,6 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Demo Dispositivos moviles'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  void _resetCounter() {
-    setState(() {
-      _counter = 0;
-    });
-  }
-
-  void _subtractCounter() {
-    setState(() {
-      _counter--;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    var logger = Logger();
-
-    logger.d("Logger is working!");
-    String svg = "assets/icons/8664845_face_grin_tongue_icon.svg";
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            SvgPicture.asset(svg, semanticsLabel: 'Acme Logo', width: 80),
-            const Text(
-              'Pulsaciones:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      persistentFooterButtons: <Widget>[
-        TextButton(onPressed: _incrementCounter, child: const Icon(Icons.add)),
-        TextButton(onPressed: _resetCounter, child: const Icon(Icons.refresh)),
-        TextButton(
-          onPressed: _subtractCounter,
-          child: const Icon(Icons.exposure_minus_1),
-        )
-      ],
     );
   }
 }
---
+++ lib/pages/my_home_page.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:laboratorio1/main.dart';
+import 'package:logger/logger.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  void _resetCounter() {
+    setState(() {
+      _counter = 0;
+    });
+  }
+
+  void _subtractCounter() {
+    setState(() {
+      _counter--;
+    });
+  }
+
+  String _icon(int counter) {
+    String default_ = "assets/icons/8664845_face_grin_tongue_icon.svg";
+    String win =
+        "assets/icons/7727475_gold cup_award_success_first_victory_icon.svg";
+    String loss = "assets/icons/7631427_status_error_alert_alarm_icon.svg";
+    if (counter == 5) {
+      return loss;
+    }
+    if (counter == 10) {
+      return win;
+    } else {
+      return default_;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var logger = Logger();
+
+    logger.d("Logger is working!");
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Card(
+            elevation: 10,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                SvgPicture.asset(_icon(_counter),
+                    semanticsLabel: 'Acme Logo', width: 80),
+                const Text(
+                  'Pulsaciones:',
+                ),
+                Text(
+                  '$_counter',
+                  style: Theme.of(context).textTheme.headlineMedium,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    TextButton(
+                      onPressed: _incrementCounter,
+                      child: const Icon(Icons.add),
+                    ),
+                    const SizedBox(width: 8),
+                    TextButton(
+                        onPressed: _resetCounter,
+                        child: const Icon(Icons.refresh)),
+                    const SizedBox(width: 8),
+                    TextButton(
+                      onPressed: _subtractCounter,
+                      child: const Icon(Icons.exposure_minus_1),
+                    ),
+                    const SizedBox(width: 8)
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
