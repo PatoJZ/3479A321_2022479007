a094d1769d84c9dd6e9f25d2c254f1f2775897a1
PatoJZ <patojimenez08@gmail.com>
2024-09-09 01:05:38 -0300

    title

--- lib/pages/my_home_page.dart
+++ lib/pages/my_home_page.dart
@@ -69,7 +69,8 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: const Text("Home"),
+        centerTitle: true,
       ),
       body: Center(
         child: Padding(
